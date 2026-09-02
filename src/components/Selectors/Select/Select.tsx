5c7f6bc964faa2920bb2c91367a6259b8cb4e7ba
maxvoices <maksslesh@gmail.com>
2026-09-02 04:37:27 +0300

    добавил стейт на селекторы

--- src/components/Selectors/Select/Select.tsx
+++ src/components/Selectors/Select/Select.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+import SelectDropdown from "./SelectDropdown";
+
 type SelectProps = {
     title: string;
     placeholder: string;
 };
 
 const Select = ({ title, placeholder }: SelectProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <>
-            <div className="select flex w-fit items-center justify-between rounded-[var(--radius-md)] bg-[var(--color-background)] p-[12px_7px_8px_7px]">
+        <div className="relative">
+            <button
+                type="button"
+                onClick={() => setIsOpen((prev) => !prev)}
+                className="select flex w-fit items-center justify-between rounded-[var(--radius-md)] bg-[var(--color-background)] p-[12px_7px_8px_7px]"
+            >
                 <div className="place-holder flex min-w-[250px] gap-3">
                     <span className="text-[14px] font-medium text-[var(--text-secondary)]">
                         {title}
@@ -22,7 +31,9 @@ const Select = ({ title, placeholder }: SelectProps) => {
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 9 5"
                         fill="none"
-                        className="h-[7px] w-[11px]"
+                        className={`h-[7px] w-[11px] transition-transform duration-200 ${
+                            isOpen ? "rotate-180" : ""
+                        }`}
                     >
                         <path
                             d="M0.5 0.5L4.5 4.5L8.5 0.5"
@@ -32,8 +43,10 @@ const Select = ({ title, placeholder }: SelectProps) => {
                         />
                     </svg>
                 </span>
-            </div>
-        </>
+            </button>
+
+            {isOpen && <SelectDropdown />}
+        </div>
     );
 };
 
